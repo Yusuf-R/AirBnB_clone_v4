debe72f01b50fabf003e2f1dc576230d2f3a027c
Yusuf-R <y.abdulwasiu@gmail.com>
2023-08-06 11:32:58 +0100

    feat: :sparkles: filter + search

--- web_dynamic/static/scripts/100-hbnb.js
+++ web_dynamic/static/scripts/100-hbnb.js
@@ -1,14 +1,4 @@
 $(document).ready(function(){
-	let amenities = {};
-	$('input[type=checkbox]').change(function() {
-		if(this.checked) {
-			amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-		}
-		else {
-			delete amenities[$(this).attr('data-id')];
-		}
-		$('div.amenities h4').text(Object.values(amenities).join(', '));
-	});
 
 url = "http://0.0.0.0:5001/api/v1/status/";
 $.get(url, function(data, textStatus) {
@@ -20,7 +10,7 @@ $.get(url, function(data, textStatus) {
 
 // search_url = "http://0.0.0.0:5001/api/v1/places_search/
 $.ajax({
-	
+
 	type: 'POST',
 	url: 'http://0.0.0.0:5001/api/v1/places_search/',
 	data: '{}',
@@ -29,8 +19,8 @@ $.ajax({
 	success: function (data) {
 		for (let i = 0; i < data.length; i++) {
 		  let place = data[i];
-		  $('.places ').append('<article><h2>' + place.name + 
-		  '</h2><div class="price_by_night"><p>' + place.price_by_night + 
+		  $('.places ').append('<article><h2>' + place.name +
+		  '</h2><div class="price_by_night"><p>' + place.price_by_night +
 		  '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
 		}
 	  }
@@ -41,7 +31,7 @@ $("button").click(function() {
 	console.log("I'm here 1");
 	data = {amenities:Object.keys(amenities)};
 $.ajax({
-	
+
 	type: 'POST',
 	url: 'http://0.0.0.0:5001/api/v1/places_search/',
 	data: JSON.stringify(data),
@@ -50,8 +40,8 @@ $.ajax({
 	success: function (data) {
 		for (let i = 0; i < data.length; i++) {
 		  let place = data[i];
-		  $('.places ').append('<article><h2>' + place.name + 
-		  '</h2><div class="price_by_night"><p>' + place.price_by_night + 
+		  $('.places ').append('<article><h2>' + place.name +
+		  '</h2><div class="price_by_night"><p>' + place.price_by_night +
 		  '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
 		}
 	  }
@@ -61,15 +51,28 @@ $.ajax({
 });
 
 
+let amenities = {};
 let states = {}
 let cities = {}
 
+$('input[type=checkbox]').change(function() {
+	if(this.checked) {
+		amenities[$(this).attr('data-id')] = $(this).attr('data-name');
+	}
+	else {
+		delete amenities[$(this).attr('data-id')];
+	}
+	$('div.amenities h4').text(Object.values(amenities).join(', '));
+});
+
+
+
 	$('div.locations li input[type=checkbox]').change(function() {
 		if(this.checked) {
-			states[$(this).attr('s-id')] = $(this).attr('s-name');
+			states[$(this).attr('data-id')] = $(this).attr('data-name');
 		}
 		else {
-			delete states[$(this).attr('s-id')];
+			delete states[$(this).attr('data-id')];
 		}
 		let result  = Object.values(states).concat(Object.values(cities));
 		$('h4').html(result.join(', '));
@@ -78,10 +81,10 @@ let cities = {}
 
 	$('div.locations l1 input[type=checkbox]').change(function() {
 		if(this.checked) {
-			cities[$(this).attr('c-id')] = $(this).attr('c-name');
+			cities[$(this).attr('data-id')] = $(this).attr('data-name');
 		}
 		else {
-			delete cities[$(this).attr('c-id')];
+			delete cities[$(this).attr('data-id')];
 		}
 		let result  = Object.values(states).concat(Object.values(cities));
 		$('h4').html(result.join(', '));
@@ -89,28 +92,29 @@ let cities = {}
 	});
 
   $("button").click(function() {
-  	console.log("I'm here 2");
   	data = {
   		amenities:Object.keys(amenities),
   		states:Object.keys(states),
   		cities:Object.keys(cities)
   	};
 
-  $.ajax({
-	 type: 'POST',
-  	url: 'http://0.0.0.0:5001/api/v1/places_search/',
-  	data:JSON.stringify(data),
-  	dataType: 'json',
-  	contentType: 'application/json',
-  	success:function (data) {
-  		for (let i = 0; i < data.length; i++) {
-  		  let place = data[i];
-  		  $('.places ').append('<article><h2>' + place.name + 
-  		  '</h2><div class="price_by_night"><p>' + place.price_by_night + 
-  		  '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
-  		      }
-        }
-  });
+		$.ajax({
+
+			type: 'POST',
+			url: 'http://0.0.0.0:5001/api/v1/places_search/',
+			data: '{}',
+			dataType: 'json',
+			contentType: 'application/json',
+			success: function (data) {
+				for (let i = 0; i < data.length; i++) {
+				  let place = data[i];
+				  $('.places ').append('<article><h2>' + place.name +
+				  '</h2><div class="price_by_night"><p>' + place.price_by_night +
+				  '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+				}
+			  }
+
+		});
 
   });
 
