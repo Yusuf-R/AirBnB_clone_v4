0951bf1db8f2fac8eeb01fb9ec7cbe8f6ce383d9
saria-mohi <70873507+saria-mohi@users.noreply.github.com>
2023-08-06 15:30:39 +0300

    Update 101-hbnb.js

--- web_dynamic/static/scripts/101-hbnb.js
+++ web_dynamic/static/scripts/101-hbnb.js
@@ -21,7 +21,12 @@ $.ajax({
 		  let place = data[i];
 		  $('.places ').append('<article><h2>' + place.name +
 		  '</h2><div class="price_by_night"><p>' + place.price_by_night +
-		  '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div></article>');
+		  '</p></div><div class="information"><div class="max_guest"><div class="guest_image"></div><p>' + place.max_guest + '</p></div><div class="number_rooms"><div class="bed_image"></div><p>' + place.number_rooms + '</p></div><div class="number_bathrooms"><div class="bath_image"></div><p>' + place.number_bathrooms + '</p></div></div><div class="description"><p>' + place.description + '</p></div>'
+		   <div class="reviews">
+		       <h2>Reviews <span class="reviewSpan" data-id="${place.id}">show</span></h2>
+			  <ul>
+			  </ul>
+		   </div>'</article>');
 		}
 	  }
 
